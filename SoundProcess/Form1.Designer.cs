14c022809b51f329bcb06304d37674be17f432e6
phantrung12 <74903981+phantrung12@users.noreply.github.com>
2021-05-22 23:28:34 +0700

    Add files via upload

---
+++ SoundProcess/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace SoundProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 516);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(317, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start the tour";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(645, 516);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(314, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop the tour";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(947, 492);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(971, 578);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SoundProcess/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace SoundProcess
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine listener = new SpeechRecognitionEngine();
+        SpeechSynthesizer Tam = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(Object sender, EventArgs e)
+        {
+            listener.SetInputToDefaultAudioDevice();
+            listener.LoadGrammar(new Grammar(new GrammarBuilder(new Choices("Hello", "Garden", "Kitchen"))));
+            listener.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Speech_recognized);
+        }
+
+        private void Speech_recognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string s = e.Result.Text;
+            switch (s)
+            {
+                case "Hello":
+                    pictureBox1.Image = Image.FromFile(@"..\..\..\Pictures\Welcome.png");
+                    break;
+                case "Garden":
+                    pictureBox1.Image = Image.FromFile(@"..\..\..\Pictures\Garden.png");
+                    break;
+                case "Kitchen":
+                    pictureBox1.Image = Image.FromFile(@"..\..\..\Pictures\Kitchen.png");
+                    break;
+                default:
+                    pictureBox1.Image = Image.FromFile(@"..\..\..\Pictures\Sorry.png");
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listener.RecognizeAsync(RecognizeMode.Multiple);
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listener.RecognizeAsyncCancel();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            pictureBox1.Image = Image.FromFile(@"..\..\..\Pictures\Choice.png");
+        }
+    }
+}
